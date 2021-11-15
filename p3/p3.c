8c788b075a7fd2732818864997f103a41b387727
sergio-legazpi <sergio.legazpi@udc.es>
2021-11-15 09:13:15 +0100

    14->Test arreglado

--- p3/p3.c
+++ p3/p3.c
@@ -107,7 +107,7 @@ void listar_vector(int v[], int n){
     printf("%4d ]",v[n-1]);
     printf("\n");
 }
-oid ord_ins(int v[], int n){
+void ord_ins(int v[], int n){
     int i, x, j;
     for(i=1;i<n;i++){
         x=v[i];
